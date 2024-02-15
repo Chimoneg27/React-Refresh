05a98da7a6e73bbe64c249f3f7ff68e06d7748ea
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-02-15 22:00:26 +0200

    Learn how to use props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <h1 className='text-3xl bg-green-500 p-3 rounded-md'>
         Vite with Tailwind
       </h1>
-      <Card />
+      <Card userName='Garvin' position='Web Developer'/>
       <Card />
       <Card />
     </>
