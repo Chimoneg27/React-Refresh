3a7c9ebfb95d5519e16dbffc2bc1b048033fe10d
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-02-17 22:28:00 +0200

    add reset button

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,22 +4,17 @@ import "./App.css"
 function App() {
   const [count, setCount] = useState(0)
 
-  const addValue = () => {
-    setCount(count + 1)
-  }
-
-  const removeValue = () => {
-    setCount(count - 1)
-  }
 
   return (
     <>
       <h1>Simple Counter, React</h1>
       <h2>Count: {count}</h2>
 
-      <button onClick={addValue}>Increase</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
       {"  "}
-      <button onClick={removeValue}>Decrease</button>
+      <button onClick={() => setCount(count -1)}>Decrease</button>
+      {"  "}
+      <button onClick={() => setCount(0)}>Reset</button>
       <p>footer: {count}</p>
     </>
   )
